c15c408ed6497e2ec725946575b2e36ec8effb07
zhangruohan59 <619811504@qq.com>
2020-04-26 20:47:52 +0800

    实现添加商品功能

--- taotao-common/src/main/java/com/taotao/constant/FTPConstant.java
+++ taotao-common/src/main/java/com/taotao/constant/FTPConstant.java
@@ -1,10 +1,10 @@
 package com.taotao.constant;
 
 public interface FTPConstant {
-    String FTP_ADDRESS = "192.168.0.109";
+    String FTP_ADDRESS = "192.168.1.19";
     int FTP_PORT = 21;
     String FTP_USERNAME = "ftpuser";
     String FTP_PASSWORD = "ftpuser";
     String FILE_UPLOAD_PATH = "/home/ftpuser/www/images";
-    String IMAGE_BASE_URL = "http://192.168.0.109/images";
+    String IMAGE_BASE_URL = "http://192.168.1.19/images";
 }
